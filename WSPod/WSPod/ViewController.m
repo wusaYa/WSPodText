71aa7cd8d38a65f0da4a70abc620925b5151337d
wusaYa <wusaYa@outlook>
2018-01-26 14:48:42 +0800

    pod text

---
+++ WSPod/WSPod/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WSPod
+//
+//  Created by 吴洒 on 2018/1/26.
+//  Copyright © 2018年 吴洒. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ WSPod/WSPod/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WSPod
+//
+//  Created by 吴洒 on 2018/1/26.
+//  Copyright © 2018年 吴洒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
